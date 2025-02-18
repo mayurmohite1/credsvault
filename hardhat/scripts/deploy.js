9f73bda24c6a6f46ba5cb05055e652e2eb13af64
M-a-y-u-r <115573684+M-a-y-u-r@users.noreply.github.com>
2025-02-18 19:05:59 +0530

    Contract deployment successful

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -3,9 +3,8 @@ const { ethers } = require("hardhat");
 async function main() {
   const CredentialsVault = await ethers.getContractFactory("CredentialsVault");
 
-  // Start deployment, returning a promise that resolves to a contract object
   const contract = await CredentialsVault.deploy();
-  await contract.waitForDeployment(); // Ensure the contract is deployed before logging the address
+  await contract.waitForDeployment();
   console.log("Contract address:", await contract.getAddress());
 }
 
